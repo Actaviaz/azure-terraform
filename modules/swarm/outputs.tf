b5fe2a2ff138943a30d1e09d4851cba0b8565bc0
Joao Furtado <joao.furtado@hpe.com>
2018-05-09 14:35:30 +0100

    Fixed the outputs for docker swarm, to provide the login info for cluster management.
    Modified the Kubernetes module to use the module for AKS instead of the Kubernetes module,
    since it's still a preview module and it's not working properly.

--- modules/swarm/outputs.tf
+++ modules/swarm/outputs.tf
@@ -1,3 +1,15 @@
 output "swarm_id" {
   value = "${azurerm_container_service.swarm.id}"
 }
+
+output "swarm_master_fqdn" {
+  value = "${lookup(azurerm_container_service.swarm.master_profile[0], "fqdn")}"
+}
+
+output "swarm_worker_fqdn" {
+  value = "${lookup(azurerm_container_service.swarm.agent_pool_profile[0], "fqdn")}"
+}
+
+output "swarm_admin_user" {
+  value = "${var.swarm_master_user}"
+}
